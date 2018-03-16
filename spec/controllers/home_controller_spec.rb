36e1c3aacfae6ca5c22d6eb9f1249df5501fcd31
sliusar <7ju7=#PG>
2018-03-16 16:55:54 +0200

    test rspec

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -3,9 +3,13 @@ require 'rails_helper'
 RSpec.describe HomeController, type: :controller do
   describe "GET index" do
     it "renders the :index template" do
-      expect(get: root_url(subdomain: nil)).to route_to(
+      expect(get: root_url).to route_to(
                                                    controller: "home",
                                                    action: "index")
+    end
+
+    it "renders the :index template" do
+      expect(get: root_url).to render_template("index2")
     end
   end
 end
