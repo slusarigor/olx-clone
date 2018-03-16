2a057269116040299404f3ee88481ef078ece661
sliusar <7ju7=#PG>
2018-03-16 17:51:02 +0200

    init rubocop

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -1,11 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe HomeController, type: :controller do
-  describe "GET index" do
-    it "renders the :index template" do
-      expect(get: root_url).to route_to(
-                                                   controller: "home",
-                                                   action: "index")
+  describe 'GET index' do
+    it 'renders the :index template' do
+      expect(get: root_url).to route_to(controller: 'home',
+                                        action: 'index')
     end
   end
 end
